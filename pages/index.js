ca4c7cfa942cfb9b6181c08ef77a47cdf4a338b3
Igor Borowski <f56JFS20ff@protonmail.com>
2021-12-11 04:43:26 +0100

    init index template

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,26 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container mx-auto">
       <Head>
         <title>Food Recipe Blog</title>
         <meta name="description" content="All tasty foods in basic recipes!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1 className='text-3xl font-bold underline'>
+      <div className='flex justify-center my-4'>
+        <div className='inline-block'>
+          <Image
+            className='rounded-full'
+            src="https://lh3.googleusercontent.com/GrQx2bXJfqWsY5J9YVQdjixy0Mi675_bCLmV10_jSPJeVLLBgHuBk3or8gb95lsMYTmZMiYT8omiZYdB_64crHtCxVdL8dEpKd1m"
+            height={150}
+            width={150}
+            alt="Recipe Food"
+          />
+        </div>
+      </div>
+      <h1 className=' text-center text-3xl font-bold'>
         Food Recipe Blog
-      </h1> 
-    
-    
-    
-    
-    
+      </h1>
     </div>
   )
 }
