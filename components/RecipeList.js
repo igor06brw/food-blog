ad34db4216f13fb10efc5bc62748c7a34e375165
Igor Borowski <f56JFS20ff@protonmail.com>
2021-12-12 01:38:16 +0100

    config list of recipes

--- components/RecipeList.js
+++ components/RecipeList.js
@@ -1,10 +1,19 @@
+import axios from 'axios'
 import React from 'react'
 import Recipe from './Recipe'
 
-export default function RecipeList() {
+
+export default function RecipeList({ recipes }) {
+
+
     return (
-        <div>
-            <Recipe />
+        <div> 
+        {
+            recipes.hits.map((recipe) => (
+                <Recipe item={recipe} key={recipe.label} />
+            ))
+        }
         </div>
     )
 }
+
