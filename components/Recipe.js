c2d34eb90d17b4ef8772dae857235d091b1594bd
Igor Borowski <f56JFS20ff@protonmail.com>
2021-12-12 01:38:32 +0100

    add test html

--- components/Recipe.js
+++ components/Recipe.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
-export default function Recipe() {
+export default function Recipe({ item }) {
+    console.log(item)
     return (
         <div>
-            
+            <Image
+                    className='rounded-full'
+                    src={item.recipe.image}
+                    height={150}
+                    width={150}
+                    alt="Recipe Food"
+                />
+            <h1>{item.recipe.label}</h1>
+
         </div>
     )
 }
