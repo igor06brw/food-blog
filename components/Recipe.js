859e1db8d1ef2f07714dbe4db18af4b29e8e1f65
Igor Borowski <f56JFS20ff@protonmail.com>
2021-12-15 15:47:06 +0100

    changes in Recipe component

--- components/Recipe.js
+++ components/Recipe.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
 
-export default function Recipe({ item }) {
-    console.log(item)
+export default function Recipe({ recipe }) {
+    console.log(recipe)
     return (
-        <div className='inline-block mx-4 text-center my-6'>
-            <Image
+        <div className='relative inline-block mx-4 text-center my-6 flex justify-center'>
+            {/* <Image
                     className='rounded-full'
                     src={item.recipe.image}
                     height={250}
                     width={250}
                     alt="Recipe Food"
-                />
-            <h1>{item.recipe.label}</h1>
-
+                /> */}
+            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-neutral-900/50 w-full h-full rounded-full flex justify-center items-center'>
+                <h1 className='text-slate-900 w-1/2'>{recipe.id}</h1>
+            </div>
         </div>
     )
 }
