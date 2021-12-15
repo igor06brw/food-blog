30e81d1f1e2f264aefd36f868ad6b782cbf81df1
Igor Borowski <f56JFS20ff@protonmail.com>
2021-12-15 15:47:38 +0100

    add fetch md files to display it in index

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,60 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
 import Header from '../components/Header'
 import Search from '../components/Search'
-import RecipeList from '../components/RecipeList'
+import { getRecipesData } from '../lib/recipes';
+import Recipe  from '../components/Recipe'
 
-export const getStaticProps = async () => {
-  const res = await fetch("https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=58db830c&app_key=794e5ae32193b0e1a3e8ded017ea9f8f")
+// export const getServerSideProps = async ({ req }) => {
+//   console.log(req.method, req.body)
 
-  const data = await res.json()
 
-  return {
-    props: {
-      data,
-    }
+//   const res = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=58db830c&app_key=794e5ae32193b0e1a3e8ded017ea9f8f`)
+//   const data = await res.json()
+
+//   return {
+//     props: {
+//       data: "xxx"
+//     }
+//   }
+
+// }
+
+
+
+export default function Home({ allRecipesData }) {
+
+  const [value, setValue] = React.useState("");
+
+  const handleChange = (e) => {
+    setValue(e)
+    console.log(e)
   }
 
-}
-
-export default function Home({ data }) {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto w-max-content">
       <Head>
-        <title>Food Recipe Blog</title>
-        <meta name="description" content="All tasty foods in basic recipes!" />
+        <title>Chicken Recipe Blog</title>
+        <meta name="description" content="All tasty chickens in basic recipes!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Search />
-      <RecipeList recipes={data} itemsPerPage={6} />
+      <Search value={value} handleChange={handleChange}/>
+      {
+        allRecipesData.allRecipesData.map((recipe) => (
+          <Recipe recipe={recipe} key={recipe.id} />
+        ))
+      }
+      
     </div>
   )
+}
+
+export const getStaticProps = () => {
+  const allRecipesData = getRecipesData();
+  return {
+    props: {
+      allRecipesData
+    }
+  }
 }
