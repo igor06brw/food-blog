cb0af3063e4ed1b58710b8a018989fdc50cf75e3
Igor Borowski <f56JFS20ff@protonmail.com>
2022-01-25 05:42:30 +0100

    something else?

--- next.config.js
+++ next.config.js
@@ -1,7 +1,35 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
 
   images: {
     domains: ['lh3.googleusercontent.com', 'www.edamam.com']
-  }
+  },
+
+  webpack5: true,
+            webpack(config) {
+                config.module.rules.push({
+                    test: /\.svg$/,
+                    use: [
+                        {
+                            loader: "@svgr/webpack",
+                            options: {
+                                titleProp: true,
+                                svgoConfig: {
+                                    plugins: {
+                                        removeViewBox: false
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                });
+                config.resolve.fallback = { fs: false };
+
+               
+               
+
+                return config;
+            }
 }
--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,6 @@ import { getRecipesData } from '../lib/recipes';
 import Pagination from '../components/Pagination';
 import Layout from '../components/Layout';
 
-// export const getServerSideProps = async ({ req }) => {
-//   console.log(req.method, req.body)
-
-
-//   const res = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=58db830c&app_key=794e5ae32193b0e1a3e8ded017ea9f8f`)
-//   const data = await res.json()
-
-//   return {
-//     props: {
-//       data: "xxx"
-//     }
-//   }
-
-// }
-
-
-
 export default function Home({ allRecipesData }) {
 
   const [value, setValue] = React.useState("");
@@ -29,12 +12,11 @@ export default function Home({ allRecipesData }) {
     setValue(e)
     console.log(e)
   }
-  console.log(allRecipesData, 'index')
+  console.log(getRecipesData, 'index')
   return (
     <Layout>
       <Search value={value} handleChange={handleChange}/>
       <Pagination items={allRecipesData.allRecipesData} itemsOnPage={3} />
-      
     </Layout>
   )
 }
