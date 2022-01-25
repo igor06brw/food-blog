c74f83a4bb19e9341b0981c5ba2e8db02b516ed8
Igor Borowski <f56JFS20ff@protonmail.com>
2022-01-25 05:40:53 +0100

    addimage next for check fetch image

--- components/Recipe.js
+++ components/Recipe.js
@@ -3,8 +3,14 @@ import React from 'react'
 
 export default function Recipe({ recipe }) {
     return (
-        <div className='relative inline-block mx-4 text-center my-6 flex justify-center'>
+        <div className='relative mx-4 text-center my-6 flex justify-center'>
            <h1 className='text-slate-900'>{recipe.slug}</h1>
+           <Image 
+            src={recipe.frontmatter.image}
+            alt="Picture of the author"
+            width={250}
+            height={250}
+           />
         </div>
     )
 }
