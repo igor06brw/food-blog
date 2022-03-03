39c4ccaddbe6d59bb4a5897e867ced3d24468d03
Igor Borowski <f56JFS20ff@protonmail.com>
2022-03-03 21:18:47 +0100

    custom pagination

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,74 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import Link from 'next/link';
-import Recipe from './Recipe';
+import Link from "next/link"
 
-export default function Pagination({ items, itemsOnPage }) {
+export default function Pagination({currentPage, numPages}) {
 
-    const [pages, setPages] = useState([]);
-    const [currentPage, setCurrentPage] = useState(0)
+    console.log(currentPage, numPages)
+    const isFirst = currentPage === 1
+    const isLast = currentPage === numPages
+    const prevPage = `/blog/page/${currentPage - 1}`
+    const nextPage = `/blog/page/${currentPage + 1}`
 
-
-    // function, which change by index
-    const handleChangePage = (page) => {
-        setCurrentPage(page)
-    }
-
+    if(numPages === 1) return <></>
     
-    useEffect(() => {
-        // assign main variables as default values
-        let currentPages = [];
-        let currentPagesIndex = 0;
-        
-        // fetch all data from index.js
-        if (items) {
-            items.forEach(element => {
-                // on each element assign to multiple arrays in array with items equal itemsOnPage
-                if (currentPages[currentPagesIndex] && currentPages[currentPagesIndex].length === itemsOnPage) currentPagesIndex++;
-
-                // in case of won't finded items {maybe problem with fetched data}, the main array is assign as empty array
-                if (currentPages[currentPagesIndex] === undefined) currentPages[currentPagesIndex] = []
-                
-                // to the first comment in forEach code: it's assign item to array
-                currentPages[currentPagesIndex].push(element)
-
-                console.log(currentPage)
-            });
-        }
-        
-        // set all separated arrays with items into state
-        setPages([...currentPages])
-    }, [items, setPages])
     
     return (
-        <div className='container'>
-
-            { /* After click button, take index -1 */ }
-            <button
-                className='relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50'
-                onClick={() => handleChangePage(currentPage - 1)}
-                disabled={currentPage === 0 ? true : false}
-                > 
-                Previous
-            </button>
-            { /* After click button, take index +1 */ }
-            <button
-                className='relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50'
-                onClick={() => handleChangePage(currentPage + 1)}
-                disabled={currentPage === pages.length - 1 ? true : false}
-                > 
-                Next
-            </button>
-            {   
-                console.log(pages),
-                pages.length && pages[currentPage] && pages[currentPage].map((item, index) => (
-                    
-                    <Link href={`/recipes/${item.slug}`} key={index}>
-                        <a className='flex flex-row justify-center'>
-                            <Recipe recipe={item} />
-                        </a>
+        <div className="mt-6">
+            <ul className="flex pl-0 list-none my-2 justify-center">
+                {!isFirst && (
+                    <Link href={prevPage}>
+                        <li className="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer">
+                            Previous
+                        </li>
                     </Link>
-                ))
-            }
+                )}
+
+                {Array.from({length: numPages}, (_, i) => (
+                    <Link href={`/blog/page/${i + 1}`}>
+                        <li className="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer">
+                            {i + 1}
+                        </li>
+                    </Link>
+                ))}
+
+
+                {!isLast && (
+                    <Link href={nextPage}>
+                        <li className="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer">
+                            Next
+                        </li>
+                    </Link>
+                )}
+            </ul>
         </div>
     )
 }
