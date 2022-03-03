edaab49344d5f379132ee26a07c9985f842e73b1
Igor Borowski <f56JFS20ff@protonmail.com>
2022-03-03 21:56:44 +0100

    improve recipe

--- components/Recipe.js
+++ components/Recipe.js
@@ -4,19 +4,23 @@ import Link from 'next/link'
 
 export default function Recipe({ recipe }) {
     return (
-        <div className='relative mx-4 text-center my-6'>
-           <h1 className='text-slate-900'>{recipe.frontmatter.title}</h1>
-           <div className='relative'>
+        <div className='relative mx-4 text-center my-6 flex flex-col justify-center items-center'>
+            <div className='flex justify-center items-center h-16'>
+                <h1 className='text-slate-900'>{recipe.frontmatter.title}</h1>
+            </div>
+            <div className='relative flex justify-center items-center'>
                 <Image 
                     src={recipe.frontmatter.image}
                     alt="Picture of the author"
                     width={250}
                     height={250}
                 />
-                <div className='absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]'>
-                    <Link href={`/blog/${recipe.slug}`}>
-                        Read more
-                    </Link>
+                <div className='absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] opacity-0 hover:opacity-100 w-full h-full flex justify-center items-center bg-slate-700/75 ease-in-out duration-150'>
+                    <div className='text-slate-200'>
+                        <Link href={`/blog/${recipe.slug}`}>
+                            Read more
+                        </Link>
+                    </div>
                 </div>
            </div>
         </div>
