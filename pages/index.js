63608831f958b10dedcebdda72f4baeee79ac7c5
Igor Borowski <f56JFS20ff@protonmail.com>
2022-03-03 21:18:29 +0100

    custom index for pages

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,65 @@
 import React, { useState } from 'react'
-import Search from '../components/Search'
-import { getRecipesData } from '../lib/recipes';
-import Pagination from '../components/Pagination';
 import Layout from '../components/Layout';
+import Recipe from '../components/Recipe';
+import Link from 'next/link';
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 
-export default function Home({ allRecipesData }) {
 
-  const [value, setValue] = React.useState("");
+export default function Home({ recipes }) {
 
-  const handleChange = (e) => {
-    setValue(e)
-    console.log(e)
-  }
-  console.log(getRecipesData, 'index')
   return (
     <Layout>
-      <Search value={value} handleChange={handleChange}/>
-      <Pagination items={allRecipesData.allRecipesData} itemsOnPage={3} />
+      <div>
+        <Link href="/blog">
+          <a className="block">
+            All Recipes
+          </a>
+        </Link> 
+      </div>
+      {
+        recipes.map((recipe, index) => 
+           <Recipe key={index} recipe={recipe} />
+        )
+      }
+
     </Layout>
   )
 }
 
-export const getStaticProps = () => {
-  const allRecipesData = getRecipesData();
+export async function getStaticProps() {
+
+  // Read all files from recipes folder
+  const files = fs.readdirSync(path.join('recipes'))
+
+
+  // Map all files from directory 
+  const recipes = files.map((filename) => {
+
+    // Assign each variable slu and replace it's name removed .md
+    const slug = filename.replace('.md','')
+
+    // Read all content from file by  filename and encode by utf-8
+    const mdWithMetaData = fs.readFileSync(
+      path.join('recipes', filename),
+      'utf-8'
+    )
+
+    // convert md file to object & string
+    const { data: frontmatter } = matter(mdWithMetaData)
+
+    // return slug & frontmatter
+    return {
+      slug,
+      frontmatter
+    }
+  })
+
+
   return {
     props: {
-      allRecipesData
+      recipes: recipes
     }
   }
 }
