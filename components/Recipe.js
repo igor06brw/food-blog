e76a92503b1f50740f0884bbeaea14a3a25da74e
Igor Borowski <f56JFS20ff@protonmail.com>
2022-03-03 21:16:53 +0100

    add custom translate for minus percent

--- components/Recipe.js
+++ components/Recipe.js
@@ -1,16 +1,24 @@
 import Image from 'next/image'
 import React from 'react'
+import Link from 'next/link'
 
 export default function Recipe({ recipe }) {
     return (
-        <div className='relative mx-4 text-center my-6 flex justify-center'>
-           <h1 className='text-slate-900'>{recipe.slug}</h1>
-           <Image 
-            src={recipe.frontmatter.image}
-            alt="Picture of the author"
-            width={250}
-            height={250}
-           />
+        <div className='relative mx-4 text-center my-6'>
+           <h1 className='text-slate-900'>{recipe.frontmatter.title}</h1>
+           <div className='relative'>
+                <Image 
+                    src={recipe.frontmatter.image}
+                    alt="Picture of the author"
+                    width={250}
+                    height={250}
+                />
+                <div className='absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]'>
+                    <Link href={`/blog/${recipe.slug}`}>
+                        Read more
+                    </Link>
+                </div>
+           </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     extend: {
       width: {
         'max-content': '960px'
+      },
+      translate: {
+        '-50%': '-50%'
       }
     },
   },
