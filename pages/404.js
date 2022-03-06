ab6435b7d025e14afc8eaae16c3e75fda10d1bbd
Igor Borowski <f56JFS20ff@protonmail.com>
2022-03-06 14:34:08 +0100

    add 404 ready component

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
+import Layout from '../components/Layout'
 
 function Error() {
   return (
-    <div>Error</div>
+    
+    <Layout>
+        <div className='flex flex-col justify-center content-center text-center'>
+            <h1 className='text-4xl my-10 text-gray-800'>Are you kidding me?! You are in wrong page.</h1>
+            <Link href="/">
+                <div className='flex justify-center'>
+                    <a className='rounded-lg bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ease-in-out duration-150'>
+                        Back
+                    </a>
+                </div>
+            </Link>
+        </div>
+    </Layout>
   )
 }
 
